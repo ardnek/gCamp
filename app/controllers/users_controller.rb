666dac074faa6db00f04050410f6b0d04cbbedbe
Kendra <kdodds@gmail.com>
2014-11-01 19:04:00 -0600

    added flash messaging to new user and edit pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     users_param=params.require(:user).permit(:first_name, :last_name, :email)
     @user=User.find(params[:id])
     if @user.update(users_param)
-      flash[:success]="User was successfully updated"
+      flash[:success]="User was successfully updated" 
     redirect_to users_path
   end
   end
