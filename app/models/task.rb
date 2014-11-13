fcad44a4102eb98e366de0b70d527abacd24d227
Kendra <kdodds@gmail.com>
2014-11-13 15:49:51 -0700

    fixed create task so users are able to create task with no date

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,11 +2,10 @@ class Task < ActiveRecord::Base
 
   validate :check_the_date, on: :create
 
-
   def check_the_date
 
-    if date < Date.today
-      errors.add(:date, "Can only be today or later.")
+    if date.present? && date < Date.today 
+      errors.add(:date, "can only be today or later.")
     end
   end
 end
--- spec/model/task_spec.rb
+++ spec/model/task_spec.rb
@@ -10,30 +10,31 @@ describe Task do
       expect(task.errors[:date].present?).to eq(true)
     end
 
-    it "success on future dates" do
+    it "success for future dates" do
       task = Task.new
       task.date = "12/12/2017"
       task.valid?
       expect(task.errors[:date].present?).to eq(false)
     end
 
-    it "success on current date" do
+    it "success for current date" do
       task = Task.new
       task.date = "11/12/2014"
       task.valid?
       expect(task.errors[:date].present?).to eq(false)
     end
 
-    it "success editing date on existing task" do
+    it "success editing date for new and existing task" do
       task = Task.create!(description: "carve pumpkins", date: "11/12/2014")
       task.date = "10/12/2014"
       task.valid?
       expect(task.errors[:date].present?).to eq(false)
+      task.date = "12/11/2014"
+      task.valid?
+      expect(task.errors[:date].present?).to eq(false)
     end
 
-    #it "editing a past task"
-    #back in time
+    #it "success editing date for an existing past task" do
     #end
-  
-
+    
 end
