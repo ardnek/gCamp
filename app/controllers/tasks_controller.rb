9f6a9f4b691a55e38dd241296792801f802e888d
Kendra <kdodds@gmail.com>
2014-11-18 16:12:21 -0700

    added pluralize and counter to Task link on project show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -79,6 +79,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:description, :complete, :due_date)
+      params.require(:task).permit(:description, :complete, :date)
     end
 end
