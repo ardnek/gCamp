6ec2c62777d5c8fde9431afc19c8266302d9554e
Kendra <kdodds@gmail.com>
2014-11-19 16:28:34 -0700

    link to projects memberships index appears on the project show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ActiveRecord::Base
 
+  has_many :memberships
+  has_many :users, through: :memberships
+
   has_many :tasks
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,21 @@ Rails.application.routes.draw do
 
   resources :projects do
     resources :tasks
+    resources :memberships
   end
   resources :users
 
 
   root "pages#index"
 
-get 'about' => 'pages#about', as: :about
-get 'terms' => 'pages#terms', as: :terms
-get 'faq' => 'pages#faq', as: :faq
+  get 'about' => 'pages#about', as: :about
+  get 'terms' => 'pages#terms', as: :terms
+  get 'faq' => 'pages#faq', as: :faq
 
-get '/sign-up' => 'registrations#new', as: :signup
-post '/sign-up' => 'registrations#create'
-get '/sign-in' => 'authentication#new', as: :signin
-post '/sign-in' => 'authentication#create'
-get '/sign-out' => 'authentication#destroy', as: :signout
+  get '/sign-up' => 'registrations#new', as: :signup
+  post '/sign-up' => 'registrations#create'
+  get '/sign-in' => 'authentication#new', as: :signin
+  post '/sign-in' => 'authentication#create'
+  get '/sign-out' => 'authentication#destroy', as: :signout
 
 end
