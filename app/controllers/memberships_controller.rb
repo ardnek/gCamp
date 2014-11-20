997f8f4400d94eb53d4b4733ae4e06f884b5f41f
Kendra <kdodds@gmail.com>
2014-11-20 14:52:41 -0700

    collection select on membership index page for users and roles

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -6,18 +6,26 @@ class MembershipsController < ApplicationController
 
 
   def index
-    @memberships=Membership.all
+    @memberships = Membership.all
     @membership = Membership.new
   end
 
+  def new
+    @membership = @project.membership.new
+  end
 
+  def create
+    @membership = @project.membership.new(membership_params)
+    @membership.save
+    redirect_to project_path(@project)
+  end
 
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  #def set_task
-  #  @membership = @project.memberships.find(params[:id])
-  #end
+  def set_task
+   @membership = @project.memberships.find(params[:id])
+  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def membership_params
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,9 @@
 class Membership < ActiveRecord::Base
 
+  validates :user, presence: :true
+
   belongs_to :project
   belongs_to :user
+  enum :role => [ :member, :owner]
 
 end
