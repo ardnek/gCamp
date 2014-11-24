ad1811b940251f9fec9d66e001a616abe44e6f8a
Kendra <kdodds@gmail.com>
2014-11-23 19:24:35 -0700

    membership index page and form for owners and members

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -6,43 +6,54 @@ class MembershipsController < ApplicationController
 
 
   def index
-    @memberships = Membership.all
     @membership = Membership.new
+    @memberships = Membership.all
+    #@memberships = @project.memberships
   end
 
-  def new
-    @membership = @project.membership.new
-  end
+  #def new
+    #@membership = @project.membership.new
+  #end
 
   def create
       @membership = @project.memberships.new(membership_params)
         if @membership.save
-            flash[:success]="Project was successfully created"
-        redirect_to action: "index"
+        redirect_to project_memberships_path, notice: 'Membership was successfully created.'
         end
       end
 
+  def edit
+    @membership = Membership.find(params[:id])
+  end
+
+
   def update
+    @membership = Membership.find(params[:id])
         if @membership.update(membership_params)
-          redirect_to action: "index", notice: 'User was successfully updated.'
+          redirect_to project_memberships_path, notice: 'Membership was successfully updated.'
+        else
+          render :index
       end
     end
 
-    #@membership = @project.membership.new(membership_params)
-    #@membership.save
-    #redirect_to project_path(@project)
-  #end
+
+  def destroy
+    membership = Membership.find(params[:id])
+    membership.destroy
+      redirect_to project_memberships_path, notice:  "Membership was successfully removed."
+  end
 
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_task
-   @membership = @project.memberships.find(params[:id])
-  end
+  #def set_task
+   #@membership = @project.memberships.find(params[:id])
+  #end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def membership_params
     params.require(:membership).permit(:project_id, :user_id, :role)
+    #params.require(:membership).permit(:user_id, :title).merge(:project_id => params[:project_id])
   end
 
 end
