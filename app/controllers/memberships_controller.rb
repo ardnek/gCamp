c4f58bf49d3a0f1c2f74cb5e2c4102b71b086759
Kendra <kdodds@gmail.com>
2014-11-23 20:44:02 -0700

    working through the membership index page and trying to get roles to save

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -7,8 +7,8 @@ class MembershipsController < ApplicationController
 
   def index
     @membership = Membership.new
-    @memberships = Membership.all
-    #@memberships = @project.memberships
+    #@memberships = Membership.all
+    @memberships = @project.memberships
   end
 
   #def new
@@ -22,37 +22,30 @@ class MembershipsController < ApplicationController
         end
       end
 
-  def edit
-    @membership = Membership.find(params[:id])
-  end
-
 
   def update
     @membership = Membership.find(params[:id])
-        if @membership.update(membership_params)
+        @membership.update(membership_params)
           redirect_to project_memberships_path, notice: 'Membership was successfully updated.'
-        else
-          render :index
-      end
     end
 
 
   def destroy
-    membership = Membership.find(params[:id])
-    membership.destroy
+    @membership = Membership.find(params[:id])
+    @membership.destroy
       redirect_to project_memberships_path, notice:  "Membership was successfully removed."
   end
 
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  #def set_task
-   #@membership = @project.memberships.find(params[:id])
-  #end
+  def set_task
+   @membership = @project.memberships.find(params[:id])
+  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def membership_params
-    params.require(:membership).permit(:project_id, :user_id, :role)
+    params.require(:membership).permit(:user_id, :role)
     #params.require(:membership).permit(:user_id, :title).merge(:project_id => params[:project_id])
   end
 
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,7 @@
 class Membership < ActiveRecord::Base
 
-  validates :user, presence: :true
+  validates :user, presence: :true, uniqueness: {scope: :project, message: "has already been added"}
+  validates :role, presence: true
 
   belongs_to :project
   belongs_to :user
