79088254dd72d496ccee85bb9bf311cb13c867c5
Kendra <kdodds@gmail.com>
2014-11-28 21:21:00 -0700

    form now passing user id into db along with current_user as user_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,24 @@
 class CommentsController < ApplicationController
 
+  #before_filter :require_user
+
   before_action do
     @project = Project.find(params[:project_id])
     @task = @project.tasks.find(params[:task_id])
   end
 
+  #def new
+    #@comment = current_user.comments.build
+  #end
+
+  #def index
+  #@comments = current_user.comments
+  #@comments = Comment.where(user_id:current_user.id)
+  #end
+
   def create
     @comment = @task.comments.new(params.require(:comment).permit(:comment, :user_id, :task_id))
-    #@comment.user_id = current_user.id
+    @comment.user_id = current_user.id
     #@comment.task_id = @task.id
 
 
