c381024100ef16843959e1e36952b4e21b617ada
Kendra <kdodds@gmail.com>
2014-12-01 16:59:24 -0700

    added bootstrap sprockets to js file

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,13 @@
+class PublicController < ApplicationController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  layout :determine_layout
+
+  private
+  def determine_layout
+    current_user ? "private" : "public"
+  end
+
+end
