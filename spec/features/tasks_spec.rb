2218b0fb4e8e7efb63c8cd346dca5015e0015858
Kendra <kdodds@gmail.com>
2014-12-02 13:56:28 -0700

    refactor capybara task spec for task description

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,63 +1,51 @@
 require 'rails_helper'
 
 feature "Tasks" do
+  before do
+    Project.create!(
+    name: "Feed Buddy",
+    )
+    User.create!(
+    first_name: "Bill",
+    last_name: "Murray",
+    email: "bill@murray.com",
+    password: "password",
+    password_confirmation: "password"
+    )
+  end
 
   scenario "User creates, edits, deletes task" do
-    visit root_path
-    click_on "Projects"
-    click_on "Create Project"
-
-    project1 = Project.create!(
-    name: "sailing trip"
-    )
-
-    click_on "Create project"
-    expect(page).to have_content("sailing trip")
-
-    visit project_tasks_path(project1)
+    visit projects_path
+    click_on "Feed Buddy"
     click_on "0 Tasks"
-    click_on "Create Task"
+    click_on "New Task"
+    fill_in "Description", with: "Buy dog food"
+    fill_in "Date", with: "12/29/2014"
+    click_button "Create Task"
+    expect(page).to have_content("Buy dog food")
+    expect(page).to have_content("Task was successfully created.")
 
-    Task.create!(
-    description: "buy boat",
-    date: "Date.today",
-    )
-    click_on "Create Task"
-
-    expect(page).to have_content("Task was successfully created")
-
-    visit project_tasks_path(project1)
-
+    click_on "Buy dog food"
     click_on "Edit"
-    fill_in "Description", with: "buy sail boat"
-    click_on "Update Task"
+    fill_in "Description", with: "dog food"
+    click_button "Update Task"
+    expect(page).to have_content("dog food")
     expect(page).to have_content("Task was successfully updated.")
-    expect(page).to have_content("buy sail boat")
 
-    click_on "Back"
-    click_on "Destroy"
+    first('.glyphicon-remove').click
     expect(page).to have_content("Task was successfully destroyed.")
-    expect(page).to have_no_content("buy sail boat")
 
   end
 
   scenario "User must enter description of a task" do
-    visit root_path
-    click_on "Projects"
-    click_on "Create Project"
-
-    project1 = Project.create!(
-    name: "Picnic basket"
-    )
-
-    click_on "Create project"
-    expect(page).to have_content("Picnic basket")
-
-    visit project_tasks_path(project1)
+    visit projects_path
+    click_on "Feed Buddy"
+    click_on "0 Tasks"
+    click_on "New Task"
+    fill_in "Description", with: ""
+    fill_in "Date", with: "12/29/2014"
     click_on "Create Task"
-
-    click_on "Create Task"
-
     expect(page).to have_content("Description can't be blank")
   end
+  
 end
