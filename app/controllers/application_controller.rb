c7eaf844a0deb60aeeb27cc63a3ba16c3f8736ad
Kendra <kdodds@gmail.com>
2014-12-01 18:21:16 -0700

    removed public controller content for capy testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-    #before_action :determine_layout
-
-    #private
-    #def determine_layout
-      #current_user ? "private" : "public"
-    #end
-
   def current_user
   User.find_by(id: session[:user_id])
   end
 
   helper_method :current_user
+
+  #before_action :determine_layout
+  #private
+  #def determine_layout
+  #current_user ? "private" : "public"
+  #end
 
 end
