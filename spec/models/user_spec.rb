4c56dc07130203b342be331a48cbfdc2d90e02fa
Kendra <kdodds@gmail.com>
2014-12-03 14:32:27 -0700

    rewrote users spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,37 +5,46 @@ describe User do
   it "validates the invalid presence of users email" do
     user = User.new
     user.valid?
-    expect(user.errors[:email].present?).to eq(true)
+    #expect(user.errors[:email].present?).to eq(true)
+    #expect(user.valid?).to be(false)
+    expect(user.errors[:email]).to be_present
   end
 
   it "validates the valid presence of users email" do
     user = User.new
     user.email = "bill@murray.com"
     user.valid?
+    #expect(user.valid?).to be(true)
     expect(user.errors[:email].present?).to eq(false)
   end
 
   it "allows user for different emails" do
-    User.create!(first_name: "Bill", last_name: "Murray", email: "bill@murray.com", password: "bill")
-    user = User.new(email: "bill@billmurray.com")
+    User.create!(first_name: "Bill", last_name: "Murray", email: "bill@murray.com",
+    password: "bill")
+    user = User.new(first_name: "Bill", last_name: "Murray", email: "bill@billmurray.com",
+    password: "bill")
     user.valid?
+    #expect(user.valid?).to be(true)
     expect(user.errors[:email].present?).to eq(false)
   end
 
-  it "fails user for duplicate emails" do
-    User.create!(first_name: "Bill", last_name: "Murray", email: "bill@murray.com", password: "bill")
-    user = User.new(email: "bill@murray.com")
+  it "rejects user for duplicate emails" do
+    User.create!(first_name: "Bill", last_name: "Murray", email: "bill@murray.com",
+    password: "bill")
+    user = User.new(first_name: "Bill", last_name: "Murray", email: "bill@murray.com",
+    password: "bill")
     user.valid?
+    #expect(user.valid?).to be(false)
     expect(user.errors[:email].present?).to eq(true)
   end
 
   it "fails user regardless of email case sensitivity" do
-    User.create!(first_name: "Bill", last_name: "Murray", email: "bill@murray.com", password: "bill")
+    User.create!(first_name: "Bill", last_name: "Murray", email: "bill@murray.com",
+    password: "bill")
     user = User.new(email: "BILL@MURRAY.com")
-    #user.valid?
-    expect(user.valid?).to be(false)
-    #expect(user.errors[:email].present?).to eq(true)
-
+    user.valid?
+    #expect(user.valid?).to be(false)
+    expect(user.errors[:email].present?).to eq(true)
   end
 
 end
