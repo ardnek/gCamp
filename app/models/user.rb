f40d64d61cb8a15766ba53358de12a81133ee16a
Kendra <kdodds@gmail.com>
2014-12-04 18:12:02 -0700

    creating object creation methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,11 @@ class User < ActiveRecord::Base
   has_many :projects, through: :memberships
   has_many :comments, dependent: :nullify
 
-  #validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates_uniqueness_of :email, presence: true, :case_sensitive => false
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  #validates_uniqueness_of :email, presence: true, :case_sensitive => false
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, presence: true
   has_secure_password
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 describe User do
 
-  it "validates the invalid presence of users email" do
-    user = User.new
+  it "is invalid without a users email" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
     expect(user.valid?).to be(false)
-    #expect(user.errors[:email].present?).to eq(true)
   end
 
-  it "validates the valid presence of users email" do
+  it "is valid with the presence of users email" do
     user = User.new
     user.email = "bill@murray.com"
     #user.valid?
@@ -47,6 +48,27 @@ describe User do
     expect(user.errors[:email].present?).to eq(true)
   end
 
+  it "is invalid with a duplicate email address" do
+    User.create!(
+    first_name: "Bill",
+    last_name: "Murray",
+    email: "bill@murray.com",
+    password: "bill",
+    password_confirmation: "bill"
+    )
+
+    user = User.new(
+    first_name: "Bill",
+    last_name: "Murray",
+    email: "bill@murray.com",
+    password: "bill",
+    password_confirmation: "bill"
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+
   it "has unique email regardless of case sensitivity" do
     user = User.create!(
     first_name: "Bill",
@@ -60,4 +82,23 @@ describe User do
     expect(user.valid?).to be(true)
     expect(user.errors[:email].present?).to eq(false)
   end
+
+  it "returns a users full name as a string" do
+    user = User.new(
+    first_name: "Bill",
+    last_name: "Murray",
+    email: "bill@murray.com",
+    password: "bill",
+    password_confirmation: "murray"
+    )
+    expect(user.full_name).to eq "Bill Murray"
+  end
+
+  it "does not allow duplicate email" do
+    user1 = create_user
+    user = User.new(email: user1.email )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
 end
