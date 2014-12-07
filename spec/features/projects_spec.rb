e758c028f58ed232142c4e013d0d631bac1c9f1c
Kendra <kdodds@gmail.com>
2014-12-06 22:54:42 -0700

    fixed capybara project & user specs

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -9,21 +9,21 @@ feature "Projects" do
     click_on "Create Project"
     fill_in "Name", with: "Screenprint tshirts"
 
-    click_on "Create project"
-    expect(page).to have_content("Project was successfully created")
+    click_on "Create Project"
+    expect(page).to have_content("Project was successfully created.")
     expect(page).to have_content("Screenprint tshirts")
 
     click_on "Screenprint tshirts"
     click_on "edit"
     fill_in "Name", with: "Screenprinting tshirts"
-    click_on "Edit Project"
+    click_on "Update Project"
+    expect(page).to have_content("Project was successfully updated.")
     expect(page).to have_content("Screenprinting tshirts")
     expect(page).to have_no_content("Screenprint tshirts")
 
     click_on "delete"
-    expect(page).to have_content("Project was successfully deleted")
+    expect(page).to have_content("Project was successfully deleted.")
     expect(page).to have_no_content("Screenprinting")
-
   end
 
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -11,19 +11,22 @@ feature "Users" do
     fill_in "Password", with: "lois"
     fill_in "Password confirmation", with: "lois"
 
-    click_on "Create user"
+    click_on "Create User"
+    expect(page).to have_content("User was successfully created.")
     expect(page).to have_content("Lois Lane")
 
     click_on "Lois Lane"
     click_on "Edit"
     fill_in "Last name", with: "Lannne"
-    click_on "Update user"
+    click_on "Update User"
+    expect(page).to have_content("User was successfully updated.")
     expect(page).to have_content("Lois Lannne")
     expect(page).to have_no_content("Lois Lane")
-  
+
     click_on "Lois Lannne"
     click_on "Edit"
     click_on "Delete user"
+    expect(page).to have_content("User was successfully deleted.")
     expect(page).to have_no_content("Lois Lannne")
   end
 
