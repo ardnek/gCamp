a4363a7edbe253c423243fe79e6b7f59b50618c1
Kendra <kdodds@gmail.com>
2014-12-20 22:57:22 -0700

    When a user creates a project they are redirected to the tasks index page for that project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,7 +18,7 @@ class ProjectsController < ApplicationController
     project_params=params.require(:project).permit(:name)
     @project = Project.new(project_params)
     if @project.save
-      redirect_to projects_path, notice: "Project was successfully created."
+      redirect_to project_tasks_path(@project), notice: "Project was successfully created."
     else
       render :new
     end
