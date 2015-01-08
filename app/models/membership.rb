d85fe32b78961f324b1f12778ad91998f45391fd
Kendra <kdodds@gmail.com>
2015-01-08 13:55:18 -0700

    Last owner cannot be removed or membership altered for last owner of a project

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,10 +1,20 @@
 class Membership < ActiveRecord::Base
 
+  before_destroy :not_last_owner?
+
   validates :user_id, presence: true, uniqueness: {scope: :project_id, message: "has already been added"}
   validates :role, presence: true
 
   belongs_to :project
   belongs_to :user
   # enum :role => [ :member, :owner]
+
+  def not_last_owner?
+    if self.role == "Member" || project.memberships.where(role: "Owner").count > 1
+      true #before_destroy will delete
+    else
+      false #before_destroy won't delete
+    end
+  end
 
 end
