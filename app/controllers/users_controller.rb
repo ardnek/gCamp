ff65f5d61ed9e4d74eab6c269c52733087b7da41
Kendra <kdodds@gmail.com>
2015-01-08 15:18:33 -0700

    Fixed edit button, so current_users can edit own user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :check_user, only: [:edit, :update]
+
   def index
     @users=User.all
   end
@@ -12,25 +14,21 @@ class UsersController < ApplicationController
     users_param=params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :pivotal_tracker_token)
     @user=User.new(users_param)
     if @user.save
-      redirect_to users_path, notice: "User was successfully created."
+      redirect_to users_path, notice: "#{@user.full_name} was successfully created."
     else
       render :new
     end
   end
 
   def edit
-    if current_user == @user
-      @user = User.find(params[:id])
-    else
-      render file: 'public/404', status: :not_found, layout: false
-    end
+    @user = User.find(params[:id])
   end
 
   def update
     users_param=params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :pivotal_tracker_token)
     @user=User.find(params[:id])
     if @user.update(users_param)
-      redirect_to users_path, notice: "User was successfully updated."
+      redirect_to users_path, notice: "#{@user.full_name} was successfully updated."
     else
       render 'edit'
     end
@@ -43,9 +41,17 @@ class UsersController < ApplicationController
   def destroy
     @user=User.find(params[:id])
     if @user.destroy
-      redirect_to users_path, notice: "User was successfully deleted."
+      redirect_to users_path, notice: "#{@user.full_name} was successfully deleted."
     end
   end
 
+  private
+
+  def check_user
+    @user = User.find(params[:id])
+    unless current_user == @user
+      render file: 'public/404', status: :not_found, layout: false
+    end
+  end
 
 end
