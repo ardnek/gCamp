4a3db5c062f25721afdc8aff206487aba326760f
Kendra <kdodds@gmail.com>
2015-03-04 01:14:37 -0700

    added notifications email false to travis.yml

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,24 +1,24 @@
-# require 'rails_helper'
-#
-# describe ApplicationController do
-#
-#   describe "permissions" do
-#     controller do
-#       def index
-#         render nothing: true
-#       end
-#     end
-#
-#     it "redirects non-logged in users to signing" do
-#       get :index
-#       expect(response).to redirect_to(signin_path)
-#     end
-#   end
-#
-#   it "allows logged-in users to see pages" do
-#     session [user_id] = create_user.id
-#       get :index
-#       expect(response).to be_success
-#     end
-#
-#   end
+require 'rails_helper'
+
+describe ApplicationController do
+
+  describe "permissions" do
+    controller do
+      def index
+        render nothing: true
+      end
+    end
+
+    it "redirects non-logged in users to signing" do
+      get :index
+      expect(response).to redirect_to(signin_path)
+    end
+  end
+
+  it "allows logged-in users to see pages" do
+    session [user_id] = create_user.id
+      get :index
+      expect(response).to be_success
+    end
+
+  end
